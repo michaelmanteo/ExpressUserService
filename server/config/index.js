f84a9a96928540a626ba024c0b5dd933602561e4
Michael N. Manteo <manteom1@hawkmail.newpaltz.edu>
2018-11-23 13:33:47 -0500

    -Installed 'dotenv' -created server/config/index.js & configured a global API_URL

---
+++ server/config/index.js
@@ -0,0 +1,6 @@
+const API_URL = process.env.NODE_ENV==='development' ? "http://localhost:3000" : "Live URL goes here";
+
+module.exports = {
+    API_URL,
+    database: 'mongodb://localhost:27017/ExpressUserService'
+}
--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
+const config = require('../server/config');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.send('Hello world!'); 
-});
+app.get('/', (req, res) => res.send('Hello world!') );
 
 module.exports = app;
